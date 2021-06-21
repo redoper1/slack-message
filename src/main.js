dc24354dc872d1d8d6efe569186d0baeb417d64d
David Rychlý <davidrychly2@gmail.com>
2021-06-21 10:27:12 +0200

    Add option to set unfurl options

--- src/main.js
+++ src/main.js
@@ -4,15 +4,27 @@ const { WebClient } = require('@slack/web-api');
 const { utils: { log } } = Apify;
 
 Apify.main(async () => {
-    const { token, channel, text, blocks, threadId } = await Apify.getValue('INPUT');
+    const { token, channel, text, blocks, threadId, unfurl } = await Apify.getValue('INPUT');
 
     const web = new WebClient(token);
+
+    let unfurlLinks = unfurl === 'links' || unfurl === 'all'
+        ? true : undefined;
+
+    let unfurlMedia = unfurl === 'media' || unfurl === 'all' ? true : undefined;
+
+    if (unfurl === 'false') {
+        unfurlLinks = false;
+        unfurlMedia = false;
+    }
 
     const result = await web.chat.postMessage({
         text,
         channel,
         blocks,
         thread_ts: threadId,
+        unfurl_links: unfurlLinks,
+        unfurl_media: unfurlMedia,
     });
 
 
